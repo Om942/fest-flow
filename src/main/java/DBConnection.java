0aceac1a4e39ebb84382bb05385f2f76375d5e43
Om Mulmule <ommulmule14@gmail.com>
2026-05-01 02:33:19 +0530

    fixed DB connection

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -2,32 +2,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-
     public static Connection getConnection() {
-
         Connection con = null;
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // Read from environment variables
             String host = System.getenv("MYSQLHOST");
             String port = System.getenv("MYSQLPORT");
             String database = System.getenv("MYSQLDATABASE");
             String user = System.getenv("MYSQLUSER");
             String password = System.getenv("MYSQLPASSWORD");
-
             String url = "jdbc:mysql://" + host + ":" + port + "/" + database +
                 "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
             con = DriverManager.getConnection(url, user, password);
-
             System.out.println("Database Connected");
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return con;
     }
 }
