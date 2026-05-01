d270bb672cd1e1b140bc6f068d6b53ba52dddf7b
Om Mulmule <ommulmule14@gmail.com>
2026-05-01 13:00:16 +0530

    hardcoded DB values

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,4 +1,3 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -6,27 +5,16 @@ public class DBConnection {
     public static Connection getConnection() {
         Connection con = null;
         try {
-        	Class.forName("com.mysql.cj.jdbc.Driver");
-
-            String host = System.getenv("MYSQLHOST") != null ? 
-                System.getenv("MYSQLHOST") : "switchyard.proxy.rlwy.net";
-            String port = System.getenv("MYSQLPORT") != null ? 
-                System.getenv("MYSQLPORT") : "19169";
-            String database = System.getenv("MYSQLDATABASE") != null ? 
-                System.getenv("MYSQLDATABASE") : "railway";
-            String user = System.getenv("MYSQLUSER") != null ? 
-                System.getenv("MYSQLUSER") : "root";
-            String password = System.getenv("MYSQLPASSWORD") != null ? 
-                System.getenv("MYSQLPASSWORD") : "rCygbYnzyHvaFlmUiJBhXisaDtKyIihA";
-
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String host = "switchyard.proxy.rlwy.net";
+            String port = "19169";
+            String database = "railway";
+            String user = "root";
+            String password = "rCygbYnzyHvaFlmUiJBhXisaDtKyIihA";
             String url = "jdbc:mysql://" + host + ":" + port + "/" + database +
                 "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
-            System.out.println("Connecting to: " + url);
-
             con = DriverManager.getConnection(url, user, password);
-            System.out.println("Database Connected Successfully!");
-
+            System.out.println("Database Connected!");
         } catch (Exception e) {
             e.printStackTrace();
         }
