78df7678f59d4337250adb0cf88b4bf63905c295
Om Mulmule <ommulmule14@gmail.com>
2026-05-01 13:10:40 +0530

    complete fresh DB fix

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,18 +1,13 @@
-import java.sql.Connection;
+ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
     public static Connection getConnection() {
         Connection con = null;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String host = "switchyard.proxy.rlwy.net";
-            String port = "19169";
-            String database = "railway";
+            String url = "jdbc:mysql://switchyard.proxy.rlwy.net:19169/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
             String user = "root";
             String password = "rCygbYnzyHvaFlmUiJBhXisaDtKyIihA";
-            String url = "jdbc:mysql://" + host + ":" + port + "/" + database +
-                "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
             con = DriverManager.getConnection(url, user, password);
             System.out.println("Database Connected!");
         } catch (Exception e) {
